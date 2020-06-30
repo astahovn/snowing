a37bf073c05c88b750523a54a0b15dde0a222625
Nikolay Astakhov <evolverhome@yandex.ru>
2020-07-01 00:53:56 +0300

    Code style

--- src/App.h
+++ src/App.h
@@ -5,12 +5,11 @@
 #include <GL\gl.h>
 #include <GL\glu.h>
 
-class App
-{
+class App {
 protected:
     HINSTANCE hInstance;
 
-    static const int N=500;
+    static const int N = 500;
 
     struct tsnow {
         float x, y, sp;
@@ -28,25 +27,33 @@ protected:
     HWND hWnd = nullptr;
     HGLRC hglrc;
     HDC hDC;
-    GLfloat angle=0, rad=3.14/180;
+    GLfloat angle = 0, rad = 3.14 / 180;
     bool keys[256];
-    bool Morphing=FALSE;
-    int StepMorph=1;
-    bool Exit=FALSE;
-    float Ex=1;
-    bool ShowMorph=TRUE;
+    bool Morphing = FALSE;
+    int StepMorph = 1;
+    bool Exit = FALSE;
+    float Ex = 1;
+    bool ShowMorph = TRUE;
 
 public:
     explicit App(HINSTANCE hInstance);
+
     ~App();
 
     void Init();
+
     void Loop();
+
     static LRESULT CALLBACK StaticWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
     LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
     void drawscene();
+
     void InitGL();
+
     void ProcessKeys();
+
     BOOL bSetupPixelFormat(HDC hdc);
 };
 
--- src/Snowing.cpp
+++ src/Snowing.cpp
@@ -1,15 +1,16 @@
 #include <windows.h>
 #include "App.h"
 
-int APIENTRY WinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPSTR     lpCmdLine,
-                     int       nCmdShow)
-{
-	App* app = new App(hInstance);
-	app->Init();
+int APIENTRY WinMain(
+        HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+        LPSTR lpCmdLine,
+        int nCmdShow
+) {
+    App *app = new App(hInstance);
+    app->Init();
     app->Loop();
 
     delete app;
-	return 0;
+    return 0;
 }
